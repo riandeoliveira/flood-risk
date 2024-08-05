a5c8025800a1e32b92840fc48ffb2e08aa7d7ace
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-08-04 23:12:32 -0300

    style: minor changes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoadingArea } from "@/components/common/loading-area";
+import { LoadingArea } from "@/components/loading-area";
 import type { ReactElement } from "react";
 import { ToastContainer } from "react-toastify";
 import VLibras from "react-vlibras";
